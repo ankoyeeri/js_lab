b852a424bdbcfbd9ce92e74b644910dc2aef8df1
ankoyeeri <ray.zug@gmail.com>
2020-10-13 10:15:05 +0300

    Lab 2. Ex 3. Done

--- Lab2/JSEx.js
+++ Lab2/JSEx.js
@@ -16,3 +16,24 @@ function ex2() {
 
     document.write("</table>")
 }
+
+function ex3() {
+    var a = prompt('Введите a:')
+    var b = prompt('Введите b:')
+    var r = Number(a)
+    document.write("<table border=1px cellpadding = 10 alighn = center>")
+    document.write("<tr><td>Радиус</td><td>Площадь круга</td><td>Длина окружности</td></tr>")
+
+    
+    do {
+        document.write("<tr>")
+        document.write("<td>" + r + "</td>")
+        document.write("<td>" + Math.PI * Math.pow(r, 2) + "</td>")
+        document.write("<td>" + 2 * Math.PI * r + "</td>")
+        document.write("</tr>")
+        r = r + 0.3
+    }
+    while (Number(r) <= b)
+
+    document.write("</table>")
+}
