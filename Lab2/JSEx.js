05e35c7bba09edad1c027477ff6ce1229abd6018
ankoyeeri <ray.zug@gmail.com>
2020-10-13 13:40:15 +0300

    Lab 2. Ex 4. Done

--- Lab2/JSEx.js
+++ Lab2/JSEx.js
@@ -24,7 +24,6 @@ function ex3() {
     document.write("<table border=1px cellpadding = 10 alighn = center>")
     document.write("<tr><td>Радиус</td><td>Площадь круга</td><td>Длина окружности</td></tr>")
 
-    
     do {
         document.write("<tr>")
         document.write("<td>" + r + "</td>")
@@ -36,4 +35,82 @@ function ex3() {
     while (Number(r) <= b)
 
     document.write("</table>")
+}
+
+function ex4() {
+    number1 = new Number(147)
+    number2 = new Number(127.18)
+
+    document.write("<table border=1px cellpadding = 10 alighn = center>")
+
+    //  Целое число
+    document.write("<tr><td>Целое число</td><td>Метод</td><td>Результат</td><td>Описание метода</td></tr>")
+
+    //  toString
+    document.write("<tr>")
+    document.write("<td>" + number1 + "</td>")
+    document.write("<td> toString(16) </td>")
+    document.write("<td>" + number1.toString(16) + "</td>")
+    document.write("<td> Строковое представление числа в 16-ричной системе счисления</td>")
+    document.write("</tr>")
+
+    //  toExponential
+    document.write("<tr>")
+    document.write("<td>" + number1 + "</td>")
+    document.write("<td> toExpotential(2) </td>")
+    document.write("<td>" + number1.toExponential(2) + "</td>")
+    document.write("<td> Вывод числа с тремя числами после точки.</td>")
+    document.write("</tr>")
+
+    //  toFixed 
+    document.write("<tr>")
+    document.write("<td>" + number1 + "</td>")
+    document.write("<td> toFixed(16) </td>")
+    document.write("<td>" + number1.toFixed(8) + "</td>")
+    document.write("<td> Представлене числа с фиксированным количеством цифр после точки </td>")
+    document.write("</tr>")
+
+    //  toPrecision
+    document.write("<tr>")
+    document.write("<td>" + number1 + "</td>")
+    document.write("<td> toPrecision(16) </td>")
+    document.write("<td>" + number1.toPrecision(2) + "</td>")
+    document.write("<td> Представление числа с заданным количеством значащих чисел</td>")
+    document.write("</tr>")
+
+    //  Число с плавающей точкой
+    document.write("<tr><td>Число с плавающей точкой</td><td>Метод</td><td>Результат</td><td>Описание метода</td></tr>")
+
+    //  toString
+    document.write("<tr>")
+    document.write("<td>" + number2 + "</td>")
+    document.write("<td> toString(16) </td>")
+    document.write("<td>" + number2.toString(16) + "</td>")
+    document.write("<td> Строковое представление числа в 16-ричной системе счисления</td>")
+    document.write("</tr>")
+
+    //  toExponential
+    document.write("<tr>")
+    document.write("<td>" + number2 + "</td>")
+    document.write("<td> toExpotential(2) </td>")
+    document.write("<td>" + number2.toExponential(2) + "</td>")
+    document.write("<td> Вывод числа с тремя числами после точки.</td>")
+    document.write("</tr>")
+
+    //  toFixed 
+    document.write("<tr>")
+    document.write("<td>" + number2 + "</td>")
+    document.write("<td> toFixed(16) </td>")
+    document.write("<td>" + number2.toFixed(8) + "</td>")
+    document.write("<td> Представлене числа с фиксированным количеством цифр после точки </td>")
+    document.write("</tr>")
+
+    //  toPrecision
+    document.write("<tr>")
+    document.write("<td>" + number2 + "</td>")
+    document.write("<td> toPrecision(16) </td>")
+    document.write("<td>" + number2.toPrecision(2) + "</td>")
+    document.write("<td> Представление числа с заданным количеством значащих чисел</td>")
+    document.write("</tr>")
+
 }
