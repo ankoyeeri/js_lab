01b9b6d1f938097209e1e57532e702628a4fbb95
ankoyeeri <ray.zug@gmail.com>
2020-10-22 17:42:38 +0300

    Lab 5. Ex 4. Not done

--- Lab5/JSEx.js
+++ Lab5/JSEx.js
@@ -1,16 +1,16 @@
-function ex1(){
-    for(let i = 0; i < document.all.length; i++){
-        alert(i + ") " + document.all[i].tagName );
+function ex1() {
+    for (let i = 0; i < document.all.length; i++) {
+        alert(i + ") " + document.all[i].tagName);
     }
 }
 
-function ex2(){
-    for(let i = 0; i < document.body.childNodes.length; i++){
+function ex2() {
+    for (let i = 0; i < document.body.childNodes.length; i++) {
         alert(document.body.childNodes[i].tagName);
     }
 }
 
-function ex3(){
+function ex3() {
     for (let i = 0; i < document.all.length; i++) {
         if (document.all[i].tagName == 'SPAN') {
             alert("Использование коллекции all: " + document.all[i].innerHTML);
@@ -19,4 +19,39 @@ function ex3(){
     }
     alert("Использование частной коллекции (span): " + document.getElementsByTagName('SPAN')[1].innerHTML);
     alert("Использование идентификатора элемента: " + document.getElementById('firstSpanId').innerHTML);
+}
+
+function ex4() {
+    // try {
+    //     let finalMark;
+    //     for (let i = 1; i < 5; i++) {
+    //         finalMark += document.getElementById('mark').innerHTML;
+    //     }
+    //     alert(finalMark);
+    // }
+    // catch (err) {
+    //     alert(err);
+    // }
+
+    try{
+        alert(document.getElementsByTagName("td").length);
+        
+        let markSum, finalMark;
+        let tagTD = document.getElementsByTagName("td");
+
+        for(let i = 0; i < tagTD.length; i++){
+            // alert(tagTD[i].innerHTML);
+            if(tagTD >= 0 && tagTD <= 10)
+            {
+                markSum += Number(tagTD[i].innerHTML);
+            }
+        }
+
+        alert(markSum);
+
+    }   
+    catch(err){
+        alert(arr);
+    }
+
 }
