dda7b3ea9b16d21242260451349cef5b39d31bfa
ankoyeeri <ray.zug@gmail.com>
2020-10-22 18:28:33 +0300

    Lab 5. Ex 4. Done

--- Lab5/JSEx.js
+++ Lab5/JSEx.js
@@ -17,40 +17,32 @@ function ex3() {
             break;
         }
     }
-    alert("Использование частной коллекции (span): " + document.getElementsByTagName('SPAN')[1].innerHTML);
-    alert("Использование идентификатора элемента: " + document.getElementById('firstSpanId').innerHTML);
+    alert("Использование частной коллекции (span): " + document.getElementsByTagName('SPAN')[0].innerHTML);
+    alert("Использование идентификатора элемента: " + document.getElementById('spanJs1').innerHTML);
 }
 
 function ex4() {
-    // try {
-    //     let finalMark;
-    //     for (let i = 1; i < 5; i++) {
-    //         finalMark += document.getElementById('mark').innerHTML;
-    //     }
-    //     alert(finalMark);
-    // }
-    // catch (err) {
-    //     alert(err);
-    // }
-
-    try{
+    try {
         alert(document.getElementsByTagName("td").length);
-        
-        let markSum, finalMark;
-        let tagTD = document.getElementsByTagName("td");
 
-        for(let i = 0; i < tagTD.length; i++){
+        let markSum = 0;
+        let finalMark = 0;
+        let tagTD = document.getElementsByTagName("td");
+        let marksArray = Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        for (let i = 0; i < tagTD.length; i++) {
             // alert(tagTD[i].innerHTML);
-            if(tagTD >= 0 && tagTD <= 10)
-            {
-                markSum += Number(tagTD[i].innerHTML);
+            for (let j = 0; j < marksArray.length; j++) {
+                if (tagTD[i].innerHTML == marksArray[j]) {
+                    markSum = markSum + Number(tagTD[i].innerHTML);
+                }
             }
         }
-
-        alert(markSum);
-
-    }   
-    catch(err){
+        finalMark = markSum / 5;
+        document.getElementById('spanJs2').innerHTML = "Средний балл: " + finalMark;
+        alert(finalMark);
+    }
+    catch (err) {
         alert(arr);
     }
 
