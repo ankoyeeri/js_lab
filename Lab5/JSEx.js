0cff6d9ba9caeb801580bbf230b6ad16c0ffc9d3
ankoyeeri <ray.zug@gmail.com>
2020-10-22 16:33:58 +0300

    Lab 5. Ex 3. Done

--- Lab5/JSEx.js
+++ Lab5/JSEx.js
@@ -9,3 +9,14 @@ function ex2(){
         alert(document.body.childNodes[i].tagName);
     }
 }
+
+function ex3(){
+    for (let i = 0; i < document.all.length; i++) {
+        if (document.all[i].tagName == 'SPAN') {
+            alert("Использование коллекции all: " + document.all[i].innerHTML);
+            break;
+        }
+    }
+    alert("Использование частной коллекции (span): " + document.getElementsByTagName('SPAN')[1].innerHTML);
+    alert("Использование идентификатора элемента: " + document.getElementById('firstSpanId').innerHTML);
+}
