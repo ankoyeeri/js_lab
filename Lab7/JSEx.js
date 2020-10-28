78d8cdcdccef9014522d28a75f935068e31e7af2
ankoyeeri <ray.zug@gmail.com>
2020-10-29 01:17:09 +0300

    Lab 7. Ex 3. Done

--- Lab7/JSEx.js
+++ Lab7/JSEx.js
@@ -1,27 +1,21 @@
 function ex3(){
+    win = open('', "window", "width=300, height=300")
+    win;
+    win.document.write("<u>");
+    win.document.write(document.forms["data"].elements[0].value);
+    win.document.write("</u> <br>"); 
+    win.document.write(`Студент ${document.forms["data"].elements[1].value} специальность ${document.forms["data"].select.value} курс 
+    ${document.forms["data"].year.value} должен сдавать следующие предметы:`); 
 
-    document.getElementById("jsPrint").innerHTML = 
-    "<u>" + 
-    document.forms["data"].elements[0].value + 
-    "</u><br>" +
-    "Студент " + 
-    document.forms["data"].elements[1].value + 
-    " специальность " +
-    document.forms["data"].select.value + 
-    " курс " + 
-    document.forms["data"].year.value + 
-    " должен сдавать следующие предметы:";
-
+    win.document.write("<ul>"); 
     for(let i = 0; i < 5; i++){
         if(document.forms["data"].subject[i].checked){
-            // document.getElementById("jsList").innerHTML = document.forms["data"].subject[i].value + "<br>";
-            // ("<li>" +document.forms["data"].subject[i].value + "</li>"); 
-            var node = document.createElement("LI");
-            var textnode = document.createTextNode(document.forms["data"].subject[i].value);
-            node.appendChild(textnode);
-            document.getElementById("jsList").appendChild(node);
+            win.document.write("<li>");
+            win.document.write(document.forms["data"].subject[i].value);
+            win.document.write("</li>");
         }
     }
+    win.document.write("</ul>");
     try{
         // alert(document.forms["data"].subject[1].value);
     }
