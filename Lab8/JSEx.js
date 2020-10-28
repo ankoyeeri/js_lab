8584e4232cd25d410f015ddb204f7adc9d124ce8
ankoyeeri <ray.zug@gmail.com>
2020-10-29 00:55:14 +0300

    Lab 8. Ex 1, 2, 3, 4, 5, 6. Done

---
+++ Lab8/JSEx.js
@@ -0,0 +1,31 @@
+function ex1(){
+    document.getElementById("text1").style.color = "red";
+}
+
+function ex1_return(){
+    document.getElementById("text1").style.color = "black";
+}
+
+function ex2(){
+    document.getElementById("text2").style.fontSize = "25px";
+}
+
+function ex3(){
+    document.getElementById("img1").src = "view.png";
+}
+
+function ex4(){
+    document.getElementById("text3").innerHTML = '<img src="retro.jpg" width="250">';
+}
+
+function ex5(){
+    document.getElementById("img2").width = "350";
+}
+
+function ex5_return(){
+    document.getElementById("img2").width = "200";
+}
+
+function ex6(){
+    document.getElementById("text4").style.border = "solid red 1px";
+}
